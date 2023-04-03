6a247183bcbcf6efb9a19d8455bca2fd5f02c4ca
KarolMakara <karol.makara19@gmail.com>
2023-04-03 19:40:04 +0200

    Initial commit

--- JPWP/settings.py
+++ JPWP/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+from JPWP.key import secret
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_!p)qb+m&3ruivc30cen_rw$#udnq)c7rarhp5y+5fs4)qddz%'
+SECRET_KEY = secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
