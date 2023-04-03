9b6482b2e4abf00fe7b3fd8a1dcfd034eb06a11d
KarolMakara <karol.makara19@gmail.com>
2023-04-03 20:01:38 +0200

    Testing if I can push to main branch

--- Accounts/models.py
+++ Accounts/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import *
+
 
 class MyUser(AbstractUser):
+    email = CharField(unique=True)
     pass
