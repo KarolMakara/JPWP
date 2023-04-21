da5842f13fb00dad605c8d80ebf25d421e3a631b
KarolMakara <karol.makara19@gmail.com>
2023-04-21 21:41:25 +0200

    merged views

---
+++ run.sh
@@ -0,0 +1,4 @@
+konsole --hold -e "redis-server" &
+konsole --hold -e "celery -A JPWP worker --loglevel=info" &
+konsole --hold -e "celery -A JPWP beat -l INFO" &
+konsole --hold -e "python3 manage.py runserver"
